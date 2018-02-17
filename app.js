603357ccbfd918db37026bcea1bae1f7acfc4cbb
Christos Panagiotakopoulos <chrispanag@gmail.com>
2018-02-17 23:40:42 +0200

    various

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-// Webserver parameter 
+// Webserver parameter
 const PORT = process.env.PORT;
 if (!PORT)
   throw new Error('missing PORT');
