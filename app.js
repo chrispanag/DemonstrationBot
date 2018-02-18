fc748b284a1ff3abe7af94ae08e12d9041509be5
Christos Panagiotakopoulos <chrispanag@gmail.com>
2018-02-18 18:06:17 +0200

    use autobind

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const { FB, webhook, messengerWebhook } = require('fblib');
 
+
 // Webserver parameter
 const PORT = process.env.PORT;
 if (!PORT)
