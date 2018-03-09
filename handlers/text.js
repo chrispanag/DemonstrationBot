e785c6c9f1be289325a227861484ba3430582ba0
Christos Panagiotakopoulos <chrispanag@gmail.com>
2018-03-09 19:51:21 +0200

    test

--- handlers/text.js
+++ handlers/text.js
@@ -2,6 +2,7 @@ const m = require('../messages/msgTemplates');
 
 function textHandler (message, id, nlp, user) {
     const msg = message.text;
+    console.log(msg);
     switch (msg.toUpperCase()) {
         case "HODOR": return m.hodorDefault(id);
         default: throw new Error("Unrecognized text Message ");
